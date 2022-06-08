17e7f0956ff14a10230eb3a7790d01cf49965bac
seolhyelin <tosiltosilkk@gmail.com>
2022-06-09 00:16:59 +0900

    Add: 맥주리스트 맵핑 세팅

--- src/routes/ListPage/index.tsx
+++ src/routes/ListPage/index.tsx
@@ -3,12 +3,37 @@ import { getBeerAPI } from 'services/beer';
 
 import styles from './listPage.module.scss';
 
+import { Search } from 'assets/svg';
+
 const page = 1;
 
 const ListPage = () => {
-  const { data } = useQuery(['beerListAPI', page], () => getBeerAPI({ page }).then((res) => console.log(res)));
+  const { data } = useQuery(['beerListAPI', page], () =>
+    getBeerAPI({ page }).then((res) => {
+      return res.data;
+    })
+  );
 
-  return <div>ListPage</div>;
+  return (
+    <div className={styles.listContainer}>
+      <section className={styles.searchBox}>
+        <label htmlFor='beer'>Search your Beer !</label>
+        <div className={styles.searchBar}>
+          <input id='beer' placeholder='gpgp' />
+          <button type='button'>
+            <Search className={styles.searchIcon} />
+          </button>
+        </div>
+      </section>
+      <section className={styles.listBox}>
+        <ul>
+          {data?.map((item) => {
+            return <li key={item.id}>{item.name}</li>;
+          })}
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default ListPage;
